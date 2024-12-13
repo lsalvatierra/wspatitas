556b7f0f101254fe94cef7b95bb56d0cfd28fec3
Administrador2 <daplicacionesmoviles1@gmail.com>
2024-12-13 12:19:25 -0500

    fix cors

--- mascotaperdida.php
+++ mascotaperdida.php
@@ -4,6 +4,11 @@ header("Access-Control-Allow-Origin: *"); // Permite solicitudes de cualquier or
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); // Métodos permitidos
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Encabezados permitidos
 header("Content-Type: application/json; charset=UTF-8");
+// Manejo de solicitud OPTIONS para CORS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
  	$res = ListarMascotasPerdidas();
  	print_r(json_encode($res));
--- persona.php
+++ persona.php
@@ -4,6 +4,11 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *"); // Permite solicitudes de cualquier origen
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); // Métodos permitidos
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Encabezados permitidos
+// Manejo de solicitud OPTIONS para CORS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $postBody = file_get_contents("php://input");
     $conver = json_decode($postBody, true);
